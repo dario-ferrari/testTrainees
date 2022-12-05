b010c3b0eb49f83db543fb69542cbbb17badd5ec
Alan Cañellas <alanjc27@gmail.com>
2022-12-05 17:40:39 -0300

    Add files via upload

--- testtrainees/test_sample.py
+++ testtrainees/test_sample.py
@@ -1,16 +1,8 @@
 
-# import requests
+import requests
 
 
-# x = requests.get('https://w3schools.com/python/demopage.htm')
+x = requests.get('https://w3schools.com/python/demopage.htm')
 
-# print(x.text)
+print(x.text)
 
-
-# content of test_sample.py
-def func(x):
-    return x + 1
-
-
-def test_answer():
-    assert func(4) == 5
